29a0a6ebd1bd1934e92660d9694a736579c9ab87
skunight@gmail.com <skunight@gmail.com>
2014-03-27 16:31:17 +0800

    web router

---
+++ routes/web.js
@@ -0,0 +1,33 @@
+/**
+ * Created by zzy on 3/26/14.
+ */
+
+var HomePageAction = require('./../action/wap/HomePageAction');
+var ProductPageAction = require('./../action/wap/ProductPageAction');
+var MemberPageAction = require('./../action/wap/MemberPageAction');
+module.exports = function(app){
+    app.all('/web/*',function(request,response,next){
+        response.charset = 'utf-8';
+        next();
+    })
+
+    app.get('/web/register',MemberPageAction.register);
+    app.post('/web/login',MemberPageAction.login);
+    app.post('/web/doRegister',MemberPageAction.doRegister);
+    app.all('/web/*',function(request,response,next){
+        if(request.session.user==null){
+            response.render('wap/login')
+        } else {
+            next();
+        }
+    })
+    app.get('/web/',HomePageAction.getHomePage);
+    app.get('/web/products/:id',ProductPageAction.getProducts);
+    app.get('/web/productDetail/:id',ProductPageAction.getDetail);
+
+
+    //ajax
+    app.get('/web/ajax/cityList',HomePageAction.cityList);
+
+    app.get('/web/ajax/hotProduct',HomePageAction.hotProduct);
+};
